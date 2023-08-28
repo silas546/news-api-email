520faae9ff847093f4c1d410bfeee6f3377cec17
Silas-Owings <silas.owings@7summitsinc.com>
2023-08-28 10:28:45 -0500

    Setup mailer and email body

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from mailer import send_email
 
 api_key = "43c6edecbcfa4159a10f99aaedaf054f"
 url = "https://newsapi.org/v2/top-headlines?"\
@@ -11,8 +12,28 @@ request = requests.get(url)
 # Convert data to dictionary
 content = request.json()
 
-# Access title/description
-for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
 
+# Construct an email body via our data
+def output_headlines():
+    output_msg = ""
+    for article in content["articles"][:11]:
+        output_msg += str(article["title"]) + "\n"
+        output_msg += str(article["description"]) + "\n"
+        output_msg += article["url"] + 2*"\n"
+    return output_msg
+
+
+raw_message = output_headlines()
+
+
+message = f"""\
+Subject: Business news headlines
+
+{raw_message}\n
+
+"""
+message = message.encode('utf8')
+print(message)
+
+
+send_email(message=message)
